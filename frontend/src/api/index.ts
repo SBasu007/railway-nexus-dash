7522e087d57e3ec0df40aa2be17b2b21fac8cd9e
NirVirat1828 <nirvikmisra@gmail.com>
2025-09-15 20:10:32 +0530

    Manually removed additional files from api_endpoint branch

--- frontend/src/api/index.ts
+++
@@ -1,7 +0,0 @@
-export * from './types';
-export { default as apiClient } from './apiClient';
-export { trainService } from './trainService';
-export { stationService } from './stationService';
-export { timetableService } from './timetableService';
-export { optimizationService } from './optimizationService';
-export { scenarioService } from './scenarioService';
